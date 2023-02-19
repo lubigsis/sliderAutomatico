7a6ed5474c26cb97a5bfc97129d5a2ebfcc41910
lubigsis <lucre.baluc@gmail.com>
2023-02-19 20:51:02 -0300

    Primer commit: CSS y algo de JS

---
+++ main.js
@@ -0,0 +1,10 @@
+//defino un array con mis imágenes y las cargo
+let imagenes = [];
+
+imagenes[0] = 'paisaje-1.jpg';
+imagenes[1] = 'paisaje-2.jpg';
+imagenes[2] = 'paisaje-3.jpg';
+imagenes[3] = 'paisaje-4.jpg';
+imagenes[4] = 'paisaje-5.jpg';
+
+document.arrayImagenes.src = imagenes[4];
