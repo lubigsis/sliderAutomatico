811884627cfe4f9e2a873e2df7a123bbc0aab695
lubigsis <lucre.baluc@gmail.com>
2023-02-20 11:37:14 -0300

    Termino el JavaScript

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//defino un array con mis imágenes y las cargo
+//----------------------------------------------defino un array vacío con mis imágenes y las cargo.
 let imagenes = [];
 
 imagenes[0] = 'paisaje-1.jpg';
@@ -6,5 +6,28 @@ imagenes[1] = 'paisaje-2.jpg';
 imagenes[2] = 'paisaje-3.jpg';
 imagenes[3] = 'paisaje-4.jpg';
 imagenes[4] = 'paisaje-5.jpg';
+imagenes[5] = 'paisaje-6.jpg';
+imagenes[6] = 'paisaje-7.jpg';
+imagenes[7] = 'paisaje-8.jpg';
+imagenes[8] = 'paisaje-9.jpg';
+imagenes[9] = 'paisaje-10.jpg';
 
-document.arrayImagenes.src = imagenes[4];
+
+/*--------------------------------------------Se crea una función p/cambiar automáticamente el índice de las img*/
+
+let indiceImagenes = 0; /*p/q' empiece del índice 0 del array*/
+let tiempo = 2000;
+
+function cambiarImagenes(){
+    document.slider.src = imagenes[indiceImagenes];
+
+    if (indiceImagenes < imagenes.length-1){
+        indiceImagenes++;
+    }else{
+        indiceImagenes = 0;
+    }
+}
+
+/*----------------------------------------------------------------------p/q' llame a la función c/cierto tiempo*/
+
+setInterval(cambiarImagenes, tiempo);
